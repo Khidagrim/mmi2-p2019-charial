937f2c7881588a16404cfe1f17e37c2ab3b3862b
Khidagrim <tristanwaluigi@orange.fr>
2019-03-24 11:33:02 +0100

    adding menu opening with its content

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,7 +1,9 @@
 let burger = document.getElementsByClassName("header__burger")[0];
+let menu = document.getElementsByClassName("header__menu")[0];
 
 burger.addEventListener("click",function(){
 
     burger.classList.toggle("header__burger--open");
+    menu.classList.toggle("header__menu--open");
 
 });
